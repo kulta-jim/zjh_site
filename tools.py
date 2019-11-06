3361cb6307dc029bec919959eb5d9384546da38c
zhang <zhangjinhua0308@163.com>
2019-11-06 23:45:03 +0800

    收集静态文件并放置于static

--- tools.py
+++ tools.py
@@ -1,10 +1,14 @@
 import re
 # import os
 import socket
+import base64
 
 from django.conf import settings
 from django.core.paginator import Paginator
 import markdown
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
 
 
 def get_pages(obj_list, page: int):
@@ -64,8 +68,21 @@ def get_ip():
     return ip, get_host_ip()
 
 
+def str_to_base64(text: str):
+    _b = base64.b64encode(bytes(text, encoding='utf-8'))
+    return _b
+
+
+def base64_to_str(base64_text: str):
+    _b = base64.b64decode(base64_text)
+    return _b
+
+
 if __name__ == '__main__':
     # replace_static_path('templates/base-index.html')
     print(get_ip())
-    print(type(get_ip()))
-
+    # print(type(get_ip()))
+    # print(str_to_base64(
+    #     'ssr://czE5Lndld2FsbC50b3A6ODA6YXV0aF9jaGFpbl9hOm5vbmU6cGxhaW46YzNWd1pYSnpjM0l1YldVLz9vYmZzcGFyYW09TmpnME1HWXhNekF3TkRJdVpXUjFMbU51JnByb3RvcGFyYW09TVRNd01EUXlPazB6U2sxNmJBJnJlbWFya3M9VTBjZ2ZGWTNmRkl3TGprNGZERTUmZ3JvdXA9Ym1WM0xuTnpjbU5tTG5SdmNB'))
+#     print(base64_to_str('c3NyOi8vY3pFNUxuZGxkMkZzYkM1MGIzQTZPREE2WVhWMGFGOWphR0ZwYmw5aE9tNXZibVU2Y0d4aGFXNDZZek5XZDFwWVNucGpNMGwxWWxkVkx6OXZZbVp6Y0dGeVlXMDlUbXBuTUUxSFdYaE5la0YzVGtSSmRWcFhVakZNYlU1MUpuQnliM1J2Y0dGeVlXMDlUVlJOZDAxRVVYbFBhekI2VTJzeE5tSkJKbkpsYldGeWEzTTlWVEJqWjJaR1dUTm1Sa2wzVEdwck5HWkVSVFVtWjNKdmRYQTlZbTFXTTB4dVRucGpiVTV0VEc1U2RtTkI='
+# ))
